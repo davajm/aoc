d05a83cd2e05f797f9c01844afe068e3d05519ed
Davi Ajmaya <davi.ajmaya@gmail.com>
2022-12-01 21:58:06 +0100

    add day 01 solution

---
+++ 01.py
@@ -0,0 +1,15 @@
+import sys
+
+totals = []
+current = 0
+
+with open('inputs/01') as f:
+    for line in f.readlines():
+        if line == '\n':
+            totals.append(current)
+            current = 0
+        else:
+            current += int(line.strip())
+
+print(f"Max: {max(totals)}")
+print(f"Sum top 3: {sum(sorted(totals)[-3:])}")
