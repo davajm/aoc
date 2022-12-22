0cdf6f8218fd7879064dd7919d574d1ec160544a
Davi Ajmaya <davi.ajmaya@kognity.com>
2022-12-22 19:50:29 +0100

    add day 20 part 2 solution

--- 2022/20.py
+++ 2022/20.py
@@ -1,21 +1,35 @@
 with open("inputs/20") as f:
-    sequence = [(int(line.rstrip("\n")), False) for line in f.readlines()]
+    values = [int(line.rstrip("\n")) for line in f.readlines()]
 
-i = 0
-while i < len(sequence):
-    n, done = sequence[i]
-    if done:
-        i += 1
-        continue
-    del sequence[i]
-    new_i = (i + n) % len(sequence)
-    if new_i == 0:
-        sequence.append((n, True))
-    else:
-        sequence.insert(new_i, (n, True))
-    if new_i < i:
-        i += 1
 
-zero = sequence.index((0, True))
-sum_grove = sum([sequence[(zero + i) % len(sequence)][0] for i in [1000, 2000, 3000]])
-print(f"Part 1: {sum_grove}")
+def mix(runs=1):
+    for _ in range(runs):
+        x = 0
+        while x < len(sequence):
+            for y, n in enumerate(sequence):
+                if n[1] == x:
+                    i = y
+                    break
+            n, orig = sequence[i]
+            del sequence[i]
+            new_i = (i + n) % len(sequence)
+            if new_i == 0:
+                sequence.append((n, orig))
+            else:
+                sequence.insert(new_i, (n, orig))
+            x += 1
+
+    zero = sequence.index((0, zero_pos))
+    pos = [1000, 2000, 3000]
+    return sum([sequence[(zero + x) % len(sequence)][0] for x in pos])
+
+
+for i, n in enumerate(values):
+    if n == 0:
+        zero_pos = i
+
+sequence = [(n, i) for i, n in enumerate(values)]
+print(f"Part 1: {mix(1)}")
+
+sequence = [(n * 811589153, i) for i, n in enumerate(values)]
+print(f"Part 2: {mix(10)}")
