d35c3babf889e13fff4f54f5260b7edc2e15babb
Davi Ajmaya <davi.ajmaya@kognity.com>
2024-12-03 16:29:09 +0100

    add day 1 solution

---
+++ 2024/01.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+# Part 1
+a, b = [], []
+
+with open("inputs/01") as f:
+    for line in f.readlines():
+        x, y = map(int, line.split())
+        a.append(x)
+        b.append(y)
+        
+
+tot = 0
+for i, j in zip(sorted(a), sorted(b)):
+    tot += abs(i - j)
+print(f"Part 1: {tot}")
+
+# Part 2
+counter_b = Counter(b)
+tot = 0
+
+for i in a:
+    tot += i * counter_b[i]
+print(f"Part 2: {tot}")
